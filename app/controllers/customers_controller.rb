2e3d39bc5dbe656e90ca2b9ba420775df2f8ce1e
Anup Neupane <anupict@gmail.com>
2014-12-16 01:00:18 +0545

    More work on customer controller and views

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,6 +2,7 @@ class CustomersController < ApplicationController
 	before_action :find_customer, only: [:show, :edit, :update, :destroy]
 
 	def index
+		@customers = Customer.all
 	end
 
 	def show
@@ -13,11 +14,14 @@ class CustomersController < ApplicationController
 
 	def create
 		 @customer = Customer.new(customer_params)
+		 @customer.save
+		 redirect_to @customer
+
 	end
 
 	def edit
 	end
-	
+
 
 	private
 
